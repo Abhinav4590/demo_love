30d83bf3083ec97a04265bd1051badcd1aa0f907
abhinav <abhinav99775@gmail.com>
2025-03-25 14:04:35 +0530

    Add files via upload

---
+++ love.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Set the background color to light blue
+st.markdown("<body style='background-color: lightblue;'></body>", unsafe_allow_html=True)
+
+# Add a title with a bold, black font
+st.markdown("<h1 style='color: cream; font-weight: bold;'>HAPPY EVERYDAY TOGETHER, MY LOVE!</h1>", unsafe_allow_html=True)
+
+
+# Add a subheading with a bold, black font
+st.markdown("<h2 style='color: white; font-weight: bold;'>You are the love of my life, my everything.</h2>", unsafe_allow_html=True)
+
+# Add a tulip flower image
+st.image("https://png.pngtree.com/png-clipart/20210530/original/pngtree-pink-cute-heart-pattern-png-image_6373677.jpg", width=300)
+
+# Add a heart-shaped bouquet image
+st.image("https://c8.alamy.com/comp/2X0BYJW/sticker-with-cute-hamster-holding-bouquet-of-red-tulips-smiling-adorable-character-in-cartoon-style-summer-spring-flowers-vector-illustration-isol-2X0BYJW.jpg", width=300)
+
+# Add a message with a bold, black font
+st.markdown("<p style='color: pink; font-weight: bold;'>You bring so much joy and love into my life. I am forever grateful to have you by my side. let's celebrate everyday as happy together , my darling wife! MY HUMI I MADE THIS PERMANENT MARK OF OUR LOVE ON INTERNET THAT'S NEVER GOING TO GET ERASED ,I AM FOREVER IN LOVE WITH YOU ,YOU ARE THE MOST alluring,appealing,attractive,exquisite,gorgeous,stunning,angelic,charming,cute,dazzling,pretty,delightful,divine,elegant,enticing,wonderful,,lovely,magnificent,picturesque,pulchritudinous,splendid,splendiferous,stunning,beauteous,glorious,sightly,snazzy,dainty,heavenly,radiant.YOUR SMILE ,YOUR EYES ,YOUR VOICE ,YOUR LAUGH,YOUR WAY OF BREATHING ,YOUR WAY OF SPEAKING ,YOUR NOSE YOUR CHEEKS YOUR HAIRS ,YOUR EYEBROWS, YOUR DIMPLES ,YHE WAY YOUR THOUGHTS ALIGN WITH MINE , THE WAY YOU JUST EXIST AND MAKE ME FEEL THE BEST, EVERYTHING ABOUT YOU IS PERFECT MY GORGEOUS!</p>", unsafe_allow_html=True)
+
+st.markdown("<p style='color: green; font-weight:'>YOUR MOTHER GAVE ME MY WORLD!""</p>", unsafe_allow_html=True)
+# Add a row of hearts
+st.markdown("<p style='color: red; font-size: 24px;'>&hearts;&hearts;&hearts;&hearts;&hearts;</p>", unsafe_allow_html=True)
+
+# Add a final message with a bold, black font
+st.markdown("<p style='color: cream; font-weight: bold;'>Forever yours, my love ......HUMAIRA X ABHINAV""</p>", unsafe_allow_html=True)
